3d78b0195ee7b725efb599a782d87c62edfb5c1d
Ivan Pribec <ivan.pribec@gmail.com>
2022-03-18 02:22:23 +0100

    Replace enorm body with norm2 intrinsic

--- src/minpack.f90
+++ src/minpack.f90
@@ -375,50 +375,7 @@ contains
         integer, intent(in) :: n !! a positive integer input variable.
         real(wp), intent(in) :: x(n) !! an input array of length n.
 
-        integer :: i
-        real(wp) :: agiant, s1, s2, s3, xabs, x1max, x3max
-
-        real(wp), parameter :: rdwarf = 3.834e-20_wp
-        real(wp), parameter :: rgiant = 1.304e19_wp
-
-        s1 = zero
-        s2 = zero
-        s3 = zero
-        x1max = zero
-        x3max = zero
-        agiant = rgiant/real(n, wp)
-        do i = 1, n
-            xabs = abs(x(i))
-            if (xabs > rdwarf .and. xabs < agiant) then
-                ! sum for intermediate components.
-                s2 = s2 + xabs**2
-            elseif (xabs <= rdwarf) then
-                ! sum for small components.
-                if (xabs <= x3max) then
-                    if (xabs /= zero) s3 = s3 + (xabs/x3max)**2
-                else
-                    s3 = one + s3*(x3max/xabs)**2
-                    x3max = xabs
-                end if
-                ! sum for large components.
-            elseif (xabs <= x1max) then
-                s1 = s1 + (xabs/x1max)**2
-            else
-                s1 = one + s1*(x1max/xabs)**2
-                x1max = xabs
-            end if
-        end do
-
-        ! calculation of norm.
-
-        if (s1 /= zero) then
-            enorm = x1max*sqrt(s1 + (s2/x1max)/x1max)
-        elseif (s2 == zero) then
-            enorm = x3max*sqrt(s3)
-        else
-            if (s2 >= x3max) enorm = sqrt(s2*(one + (x3max/s2)*(x3max*s3)))
-            if (s2 < x3max) enorm = sqrt(x3max*((s2/x3max) + (x3max*s3)))
-        end if
+        enorm = norm2(x(1:n))
 
     end function enorm
 !*****************************************************************************************
